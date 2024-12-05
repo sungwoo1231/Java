30c22833c2c1776bbf252ce918dcd29b62dd6a29
sungwoo1231 <tjddn159159@naver.com>
2024-12-05 17:11:50 +0900

    .

--- CodingTest/src/CodingTest_1/Vehicle.java
+++ CodingTest/src/CodingTest_1/Vehicle.java
@@ -8,6 +8,23 @@ abstract public class Vehicle {
         this.modelName = modelName;
         MaxSpeed = maxSpeed;
     }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public int getMaxSpeed() {
+        return MaxSpeed;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        MaxSpeed = maxSpeed;
+    }
+
     abstract void drive ();
     final void displayInfo(){
         System.out.println("Model : " +modelName+ "," + " MaxSpeed : " +MaxSpeed);
